3c4d388e3a8949e176542c8986249fcb559f2520
Henrique Tavares <ihenrits@gmail.com>
2020-04-23 19:37:37 -0300

    Pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import SignIn from '~/pages/SignIn';
+
 import GlobalStyle from '~/styles/global';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <h1>Go barber Web</h1>
+    <SignIn />
   </>
 );
 
