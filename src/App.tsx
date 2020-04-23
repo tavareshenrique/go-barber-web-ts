7b072eae848e346455a38051314ac552b99a741b
Henrique Tavares <ihenrits@gmail.com>
2020-04-23 17:34:32 -0300

    Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Go barber Web</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <h1>Go barber Web</h1>
+  </>
+);
 
 export default App;
