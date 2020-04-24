98b5e3ad9481c0a89771d84ec818310931724a26
Henrique Tavares <ihenrits@gmail.com>
2020-04-24 16:28:30 -0300

    Estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { AuthProvider } from '~/hooks/AuthContext';
 
+import ToastContainer from '~/components/ToastContainer';
+
 import SignIn from '~/pages/SignIn';
 // import SignUp from '~/pages/SignUp';
 
@@ -12,6 +14,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -46,8 +46,10 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (err) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn],
