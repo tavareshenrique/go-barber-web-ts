9f0f1a3e7863f75534081924bfa03fe3f7e9ecef
Henrique Tavares <ihenrits@gmail.com>
2020-05-23 17:09:48 -0300

    Proximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 
 import { useAuth } from '~/hooks/AuthContext';
 
 import logoImg from '~/assets/logo.svg';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextApointment,
+  Calendar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
@@ -30,6 +39,33 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-Feira</span>
+          </p>
+
+          <NextApointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/27022914?s=460&u=8beafca4ba6994b2c6fc1012008a8b0c007380d9&v=4"
+                alt="Henrique Tavares"
+              />
+              <strong>Henrique Tavares</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextApointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
